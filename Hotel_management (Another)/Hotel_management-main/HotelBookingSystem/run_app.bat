9b9c96415bfc91fd258810d01021d5ee1e5a0780
Anannya AK <anannyaak01@gmail.com>
2026-06-27 00:22:25 +0530

    Delete Hotel_management (Another)/Hotel_management-main/HotelBookingSystem/run_app.bat

--- Hotel_management (Another)/Hotel_management-main/HotelBookingSystem/run_app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\user\Hotel_management (Another)\Hotel_management-main\HotelBookingSystem"
-java -cp "out;src\lib\mysql-connector-j-9.4.0.jar" Main
-echo java exit code %ERRORLEVEL%
