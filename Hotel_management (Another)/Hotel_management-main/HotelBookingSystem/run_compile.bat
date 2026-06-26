c3c4936b525af8c3863c435e702944564c93929d
Anannya AK <anannyaak01@gmail.com>
2026-06-27 00:56:53 +0530

    Remove generated files

--- Hotel_management (Another)/Hotel_management-main/HotelBookingSystem/run_compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\user\Hotel_management (Another)\Hotel_management-main\HotelBookingSystem\src"
-javac -d "..\out" -cp ".;lib\\mysql-connector-j-9.4.0.jar" Main.java ui\\*.java db\\*.java model\\*.java
-echo javac exit code %ERRORLEVEL%
